77114e57d09fd45cc904b484381b3bf4798bbad1
jungsae <wjdtpgks388@gmail.com>
2023-11-22 11:34:57 +0900

    [ADD]

---
+++ basic/AuthorizationPrac.sql
@@ -0,0 +1,16 @@
+-- 유저조회
+select * from MYSQL.user;
+-- 유저삭제
+drop user 'test@localhost';
+-- 유저생성
+create user 'test@localhost' identified by 'testpw';
+-- 권한부여
+grant select on board.author to 'test@localhost';
+grant insert on board.author to 'test@localhost';
+-- 권한지우기
+revoke select on board.author from 'test@localhost';
+
+FLUSH PRIVILEGES;
+
+-- 권한보기
+show grants for 'test@localhost';
