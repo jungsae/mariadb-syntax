b7e7fa3623498414bc8406cd74b9b4a7c8d68acc
jungsae <wjdtpgks388@gmail.com>
2023-11-22 11:36:02 +0900

    [FIX]

--- basic/AuthorizationPrac.sql
+++ basic/AuthorizationPrac.sql
@@ -9,7 +9,7 @@ grant select on board.author to 'test@localhost';
 grant insert on board.author to 'test@localhost';
 -- 권한지우기
 revoke select on board.author from 'test@localhost';
-
+-- 권한 변경사항 적용
 FLUSH PRIVILEGES;
 
 -- 권한보기
